fe6b4145358437b8e4a1c1a10bad7a50b9906f1b
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-08-09 14:03:17 -0400

    adding example

--- Session_05/Examples/Example_01/js/vis.js
+++ Session_05/Examples/Example_01/js/vis.js
@@ -1,9 +1,54 @@
-var w = 960,
-	h = 500;
+var width = 960,
+    height = 500;
 
-var projection = d3.geo.albersUsa()
-	.scale(1000)
-	.translate([w/2,h/2]);
+var _x = d3.scale.linear()
+    .domain([0, width])
+    .range([-180, 180]);
+
+var _y = d3.scale.linear()
+    .domain([0, height])
+    .range([90, -90]);
+
+var projection = d3.geo.conicEqualArea()
+    .scale(150)
+    .center([0, 33])
+    .translate([width * .5, height * .5])
+    .precision(.3);
+
+var canvas = d3.select("body").append("canvas")
+    .attr("width", width)
+    .attr("height", height);
+
+var context = canvas.node().getContext("2d");
 
 var path = d3.geo.path()
-	.projection(projection);
+    .projection(projection)
+    .context(context);
+
+d3.json("data/world-110m.json", function(error, world) {
+  if (error) throw error;
+
+  var land = topojson.feature(world, world.objects.land),
+      sphere = {type: "Sphere"},
+      touch = "ontouchstart" in window;
+
+  canvas.on(touch ? "touchmove" : "mousemove", move);
+
+  draw();
+
+  function move() {
+    var p = touch ? d3.touches(this)[0] : d3.mouse(this);
+    projection.rotate([_x(p[0]), _y(p[1])]), draw();
+    d3.event.preventDefault();
+  }
+
+  function draw() {
+    context.clearRect(0, 0, width, height);
+    context.beginPath();
+    path(land);
+    context.fill();
+    context.beginPath();
+    path(sphere);
+    context.stroke();
+  }
+});
