534db9f8748c36b1086f9bb618ed638349518bc8
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-12-02 02:02:16 -0500

    adding examples

--- 2016_Fall/Session_09/Examples/Example_01_BLANK/vis.js
+++ 2016_Fall/Session_09/Examples/Example_01_BLANK/vis.js
@@ -1,10 +1,12 @@
 /* ------------------------------------------------------------------
- EXAMPLE 01
+ EXAMPLE 01 [BLANK]
  -------------------------------------------------------------------- */
 
+//preset width and height
 var w = window.innerWidth,
 		h = window.innerHeight;
 
+//define variables for retrieved data
 var data_map,
 		data_galleries;
 
@@ -13,54 +15,27 @@ var svg = d3.select('#container').append('svg')
 	.attr('width',w)
 	.attr('height',h);
 
+/*	-----------------------------------------
+		YOUR CODE GOES INSIDE THIS FUNCTION BELOW
+		----------------------------------------- */
+
 function generate(){
 
 	//define projection
-	var projection = d3.geo.mercator()
-		.center([-74.25,40.69])
-		.scale(70000)
-		.translate([w/4,h/2]);
 
 	//define path function
-	var path = d3.geo.path()
-		.projection(projection);
 
 	//create map group
-	var map_g = svg.append('g');
 
 	//grab features from the GeoJSON dataset (5 boroughs)
-	var features = data_map.features;
 
 	//draw map
-	var map;
-	map = map_g.selectAll('path.boroughs')
-		.data(features);
-	map.enter()
-		.append('path')
-		.classed('boroughs',true)
-		;
-	map.attr('d',path);
-	map.exit().remove();
 
 	//use projection to plot galleries
-	var galleries;
-	galleries = map_g.selectAll('circle.pin')
-		.data(data_galleries);
-	galleries.enter().append('circle')
-		.classed('pin',true);
-	galleries
-		.attr('cx',function(d){
-			var coords = projection([+d.lon,+d.lat]);
-			return coords[0];
-		})
-		.attr('cy',function(d){
-			var coords = projection([+d.lon,+d.lat]);
-			return coords[1];
-		})
-		.attr('r',2);
-	galleries.exit().remove();
 }
 
+//this function retrieves our CSV and GeoJSON files
+//once it's finished, it calls generate() to render the retrieved data
 function get_data(_callback){
 	var loading = [1,2];
 
